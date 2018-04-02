b45b827b782de3de21a9df2bf42a060c03edce46
Jheyu <g615120@gmail.com>
2018-04-02 15:23:36 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{ route('about') }}">關於</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.default')
-@section('title', 'Laravel Sample Project')
 
 @section('content')
-  <h1>首頁</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      Laravel Sample Project
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">現在註冊</a>
+    </p>
+  </div>
 @stop
